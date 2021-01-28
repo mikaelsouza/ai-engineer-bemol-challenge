2a3f6609938a3ff7ce9157ef76122c9cb4ee83fd
Mikael Souza <mikael@icomp.ufam.edu.br>
2021-01-28 12:15:03 -0400

    [WIP] Finalizado esboço da API

--- src/api/client.py
+++ src/api/client.py
@@ -1,15 +1,54 @@
 import pandas as pd
+import pickle
 from pathlib import Path
 from typing import Union
 
 
 class ClientInformer(object):
-    def __init__(self, path: Union[Path, str]) -> None:
+    def __init__(self, data_path: Union[Path, str], country_path: Union[Path,
+                                                                        str],
+                 client_path: Union[Path, str]) -> None:
         super().__init__()
-        self.path = path
-        self.data = pd.read_csv(self.path)
+        # Setando caminhos
+        self.data_path = data_path
+        self.contry_path = country_path
+        self.client_path = client_path
 
-        self.countries = self.data['Country'].unique()
+        # Carregando dados:
+        self.data = pd.read_csv(self.data_path)
+        with open(country_path, 'rb') as f:
+            self.country_labels = pickle.load(f)
+        with open(client_path, 'rb') as f:
+            self.client_labels = pickle.load(f)
 
     def get_countries(self):
+        '''
+        Obtém o ID de todos os países existentes na base de dados.
+        '''
+        return self.country_labels
 
+    def get_clients_by_country(self, country_id: int):
+        '''
+        Dado o ID de um país, retorna dados de todos os clientes do país especificado
+        '''
+        return self.data[self.data['Country'] == country_id].to_json(
+            orient='records')
+
+    def get_client_by_id(self, client_id: int):
+        '''
+        Dado o ID de um cliente, retorna dados daquele cliente
+        '''
+        return self.data[self.data['CustomerID'] == client_id].to_json(
+            orient='records')
+
+    def get_tiers(self):
+        '''
+        Obtém o ID de todos os tiers de clientes da base de dados
+        '''
+        return self.client_labels
+
+    def get_client_by_tier(self, tier: int):
+        '''
+        Dado o tier de cliente, obtém todos os clientes contendo aquele tier
+        '''
+        return self.data[self.data['Label'] == tier].to_json(orient='records')
--- src/api/main.py
+++ src/api/main.py
@@ -1,6 +1,25 @@
+import client
 from flask import Flask
 
 app = Flask(__name__)
+
+data_path = 'data/processed/confidential_rfm.csv'
+client_path = 'data/bin/CLIENTLABELS.BIN'
+country_path = 'data/bin/COUNTRYIDS.BIN'
+
+client_informer = client.ClientInformer(data_path=data_path,
+                                        client_path=client_path,
+                                        country_path=country_path)
+
+
+def sane_input(id: str) -> int:
+
+    try:
+        id = int(id)
+    except ValueError:
+        print("Valor do tipo incorreto. Por favor insira um valor numérico.")
+        id = None
+    return id
 
 
 @app.route('/')
@@ -10,4 +29,27 @@ def hello():
 
 @app.route('/country/<id>')
 def get_clients_by_country(id):
-    return {'data': "oi rs"}
+    id = sane_input(id)
+    return {'data': client_informer.get_clients_by_country(id)}
+
+
+@app.route('/countries/')
+def get_countries():
+    return {'data': client_informer.get_countries()}
+
+
+@app.route('/client/<id>')
+def get_client_by_id(id):
+    id = sane_input(id)
+    return {'data': client_informer.get_client_by_id(id)}
+
+
+@app.route('/tiers/')
+def get_tiers():
+    return {'data': client_informer.get_tiers()}
+
+
+@app.route('/tier/<id>')
+def get_client_by_tier(id):
+    id = sane_input(id)
+    return {'data': client_informer.get_client_by_tier(id)}
